2eb4f0e6f0a1bd08e3a1c36b3e4456982a0a7ebe
Anastasiia <anastasiia.kornatska@gmail.com>
2022-04-18 19:46:16 -0700

    Created new project for tic-tac-toe game

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Game {
+
+    public static void main(String[] args) {
+
+        System.out.println("----------------");
+        System.out.println("|    |    |    |");
+        System.out.println("----------------");
+        System.out.println("|    |    |    |");
+        System.out.println("----------------");
+        System.out.println("|    |    |    |");
+        System.out.println("----------------");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Player 1 - enter 1-3 for line, 1-3 for column:");
+        int player1line = scan.nextInt();
+        int player1column = scan.nextInt();
+
+    }
 }
